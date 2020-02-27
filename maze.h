2e06f3f255aa9ffef919746b1fbf96909ca02e16
Willem S. Klein Wassink '21 <wwassink@flume.cs.dartmouth.edu>
2020-02-26 21:24:01 -0500

    2/26, WillemKW, Some updates to maze.c and maze.h. Added internal variablesand 'mazenode_new' method. Doesn't do anything.

--- maze.h
+++ maze.h
@@ -1,10 +1,12 @@
 /*
+ * Willem Klein Wassink, CS50, Winter 2020
+ * 
  * maze.h - header file for the client-side procedurally created maze
  * 
  * A maze is a 2d array of mazenode structs. It starts with all nodes
  * being null and creates them as an avatar requests to move into a place.
  * 
- * A mazenode is an element of a maze representing one square.
+ * A mazenode is an element of a maze representing one square. See maze.c for more information
  * 
  */
 
@@ -13,3 +15,7 @@
 
 // ------------------- global types
 typedef struct maze maze_t;
+
+// ------------------- functions
+
+#endif // __MAZE_H

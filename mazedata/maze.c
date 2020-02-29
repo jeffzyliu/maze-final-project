1b027cc0bcc84d88af23cc2599d284d131df967f
Willem S. Klein Wassink '21 <wwassink@flume.cs.dartmouth.edu>
2020-02-28 22:48:08 -0500

    added delete methods

--- mazedata/maze.c
+++ mazedata/maze.c
@@ -8,49 +8,64 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include "../amazing.h"
+#include "maze.h"
 
-// ------------------- local types
+// ------------------- global types
 typedef struct mazenode {
-    // (unexplored = null)
-    // (open space = pointer to next node)
-    // (wall = pointer to "wall node") TODO: How should this be identified?
-    mazenode_t *north;
-    mazenode_t *south;
-    mazenode_t *east;
-    mazenode_t *west;
+    // Array of 4 mazenodes. Index 0 is west, 1 is north, 2 is south, 3 is east
+        // (unexplored = null)
+        // (open space = pointer to next node)
+        // (wall = pointer to self)
+    mazenode_t *neighbors[4];
+
+    // For checking if the node contains an avatar.
+    // -1 for no avatar. Otherwise, it's the avatar's ID.
+    int avatar;
 
     // (x,y) coordnates
     int x;
     int y;
 } mazenode_t;
 
-// ------------------- global types
+
 typedef struct maze {
     int height;
     int width;
-    mazenode_t ***array;
+    mazenode_t ***array;    // 2d array of mazenodes. Index like this: array[y][x]
 } maze_t;
+
 
 // ------------------- local functions prototypes
 static mazenode_t *mazenode_new(int x, int y);
 
+
 // ------------------- local functions
 // Initalize a new mazenode & return it. NULL on failure. Caller must delete
+// Neighbors init to NULL, caller gives (x,y), avatar init to -1
 static mazenode_t *mazenode_new(int x, int y) {
     mazenode_t *node = malloc(sizeof(mazenode_t));
 
     if (node == NULL) {
         return NULL;
     } else {
+        node->neighbors[M_WEST] = NULL;
+        node->neighbors[M_NORTH] = NULL;
+        node->neighbors[M_SOUTH] = NULL;
+        node->neighbors[M_EAST] = NULL;
         node->x = x;
         node->y = y;
-        node->north = NULL;
-        node->south = NULL;
-        node->east = NULL;
-        node->west = NULL;
+        node->avatar = -1;
         return node;
     }
 }
+
+
+// Frees contents of a mazenode
+static void mazenode_delete(mazenode_t *node) {
+    free(node);
+}
+
 
 // ------------------- global functions
 // Creates a new maze of set width and height, filled with mazenodes. Caller has to delete it.
@@ -74,12 +89,49 @@ maze_t *maze_new(int height, int width) {
     }
 }
 
-bool set_wall(maze_t maze, int x, int y, const int d) {
-    mazenode_t node = maze->array[x][y];
-    switch (direction) {
-        case M_NORTH: // Get these from amazing.h
-        case M_SOUTH: 
-        case M_EAST:
-        case M_WEST:
+
+// Caller passes in the maze, (x,y) coordinates for a location in it, and a pointer to the new neighbor.
+// unexplored = null, open space = pointer to next node, wall = pointer to self
+// 0 for West, 1 for North, 2 for South, 3 for East.
+// Returns pointer to the updated node if success, NULL if failure
+mazenode_t *set_neighbor(maze_t *maze, int x, int y, const int d, mazenode_t *new_neighbor) {
+    if (d < 0 || d > 3) {
+        fprintf(stderr, "Error: 'd' must be 0 (West), 1 (North), 2 (South), or 3 (East)");
+        return NULL;
     }
+    mazenode_t *node = maze->array[y][x];
+    node->neighbors[d] = new_neighbor;
+    return node;
 }
+
+
+// Caller passes in maze and (x,y) coords for a node to be examined
+// Returns number of neighbors in the node that are walls.
+int wall_count(maze_t *maze, int x, int y) {
+    if (maze == NULL) {
+        fprintf("Error: NULL maze passed in");
+        return NULL;
+    }
+    mazenode_t *node = maze->array[x][y];
+    int wall_count = 0;
+    for (int i=0; i<4; i++) {
+        if (node->neighbors[i] == node) {
+            wall_count++;
+        }
+    }
+    return wall_count;
+}
+
+
+// Frees all the mazenodes in a maze
+void maze_delete(maze_t *maze) {
+    if (maze != NULL) {
+        for (int x=0; x < maze->width; x++) {
+            for (int y=0; y < maze->height; y++) {
+                if (maze->array[y][x] != NULL) {
+                    mazenode_delete(maze->array[y][x]);
+                }
+            }
+        }
+        free(maze);
+    }
--- mazedata/maze.h
+++ mazedata/maze.h
@@ -10,6 +10,8 @@
  * 
  */
 
+#include "../amazing.h"
+
 #ifndef __MAZE_H
 #define __MAZE_H
 
@@ -17,5 +19,57 @@
 typedef struct maze maze_t;
 
 // ------------------- functions
+
+// ------------------- maze_new
+/* Initializes a new maze
+ * 
+ * Inputs:
+ *      An integer width and height for the maze
+ * 
+ * Outputs:
+ *      Pointer to new maze, or NULL if error.
+ * 
+ * Caller responsibilities:
+ *      Deleting the maze later with maze_delete
+ */
+maze_t *maze_new(int height, int width);
+
+// ------------------- set_wall
+/* Sets a maze's neighbor to a passed-in node
+ * 
+ * Inputs:
+ *      a maze pointer
+ *      integer (x,y) coordinates
+ *      an integer 'd' representing a direction
+ *      the contents of the new neighbor
+ * 
+ * Outputs:
+ *      NULL if failure
+ *      otherwise, a pointer to the updated node
+ */
+mazenode_t *set_neighbor(maze_t *maze, int x, int y, const int d, mazenode_t *new_neighbor);
+
+// ------------------- wall_count
+/* Returns the number of walls a mazenode is neighbors with
+ *
+ * Inputs:
+ *      a pointer to a maze
+ *      integer (x,y) coordinates
+ * 
+ * Outputs:
+ *      NULL if failure
+ *      number of walls in a node on success
+ */
+int wall_count(maze_t *maze, int x, int y);
+
+// ------------------- maze_delete
+/* Deletes all the mazenodes in a maze and then the maze itself
+ * 
+ * Inputs:
+ *      a pointer to a valid maze
+ * 
+ * Ignores NULL mazes    
+ */
+void maze_delete(maze_t *maze);
 
 #endif // __MAZE_H

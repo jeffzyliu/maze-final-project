759254b920f6b892be84927484792f2403fc36fb
jeffzyliu <jeffzyliu@gmail.com>
2020-03-05 00:41:02 -0500

    what

--- algorithms/move.c
+++ algorithms/move.c
@@ -76,7 +76,7 @@ void maze_update(int lastHeading, XYPos oldLoc, XYPos newLoc, maze_t *maze, int 
     set_avatar(maze, oldLoc.x, oldLoc.y, avatarID, false);
     set_avatar(maze, newLoc.x, newLoc.y, avatarID, true);
     if (direction != M_NULL_MOVE) { // moved in a direction, set new path in direction moved
-        if (wall_count(maze, oldLoc.x, oldLoc.y) >= 3/* && get_avatar(maze, oldLoc.x, oldLoc.y) == -1*/) { // exited a dead-end, mark as closed
+        if (wall_count(maze, oldLoc.x, oldLoc.y) >= 3 && get_avatar(maze, oldLoc.x, oldLoc.y) == -1) { // exited a dead-end, mark as closed
             printf("supposed dead end at %d,%d, getavatar =  %d\n", oldLoc.x, oldLoc.y, get_avatar(maze, oldLoc.x, oldLoc.y));
             // check for contains avatar to not trap anyone behind
             set_neighbor(maze, oldLoc.x, oldLoc.y, direction, oldLoc.x, oldLoc.y); // cannot go forward
